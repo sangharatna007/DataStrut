13c3c0dc6bc81b87d452413ef7c1544b11f3022d
Sangharatna <sangharatna007@gmail.com>
2018-07-28 08:08:07 +0530

    all requied spring boot files and dependenics

---
+++ SpringWithHibernateReact/src/main/java/com/spring/hibernate/SpringWithHibernateReact/SpringWithHibernateReactApplication.java
@@ -0,0 +1,12 @@
+package com.spring.hibernate.SpringWithHibernateReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithHibernateReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithHibernateReactApplication.class, args);
+	}
+}
